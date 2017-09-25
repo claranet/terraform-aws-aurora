133dc9a7649e3ebf92907af8961126e5ae860143
Sam Bashton <sam.bashton@claranet.uk>
2017-09-25 16:55:16 +0100

    Cleanup: Variable descriptions and generated README

--- variables.tf
+++ variables.tf
@@ -1,19 +1,32 @@
-variable "name" {}
-
-variable "subnets" {
-  type = "list"
+variable "name" {
+  type        = "string"
+  description = "Name given to DB subnet group"
 }
 
-variable "envname" {}
+variable "subnets" {
+  type        = "list"
+  description = "List of subnet IDs to use"
+}
 
-variable "envtype" {}
+variable "envname" {
+  type        = "string"
+  description = "Environment name (eg,test, stage or prod)"
+}
+
+variable "envtype" {
+  type        = "string"
+  description = "Environment type (eg,prod or nonprod)"
+}
 
 variable "azs" {
-  type = "list"
+  type        = "list"
+  description = "List of AZs to use"
 }
 
 variable "replica_count" {
-  default = "0"
+  type        = "string"
+  default     = "0"
+  description = "Number of reader nodes to create"
 }
 
 variable "security_groups" {
@@ -22,87 +35,131 @@ variable "security_groups" {
 }
 
 variable "instance_type" {
-  default = "db.t2.small"
+  type        = "string"
+  default     = "db.t2.small"
+  description = "Instance type to use"
 }
 
 variable "publicly_accessible" {
-  default = "false"
+  type        = "string"
+  default     = "false"
+  description = "Whether the DB should have a public IP address"
 }
 
 variable "username" {
-  default = "root"
+  default     = "root"
+  description = "Master DB username"
 }
 
-variable "password" {}
+variable "password" {
+  type        = "string"
+  description = "Master DB password"
+}
 
 variable "final_snapshot_identifier" {
-  default = "final_snapshot"
+  type        = "string"
+  default     = "final_snapshot"
+  description = "The name to use when creating a final snapshot on cluster destroy, appends a random 8 digits to name to ensure it's unique too."
 }
 
 variable "skip_final_snapshot" {
-  default = "false"
+  type        = "string"
+  default     = "false"
+  description = "Should a final snapshot be created on cluster destroy"
 }
 
 variable "backup_retention_period" {
-  default = "0"
+  type        = "string"
+  default     = "0"
+  description = "How long to keep backups for (in days)"
 }
 
 variable "preferred_backup_window" {
-  default = "02:00-03:00"
+  type        = "string"
+  default     = "02:00-03:00"
+  description = "When to perform DB backups"
 }
 
 variable "preferred_maintenance_window" {
-  default = "sun:05:00-sun:06:00"
+  type        = "string"
+  default     = "sun:05:00-sun:06:00"
+  description = "When to perform DB maintenance"
 }
 
 variable "port" {
-  default = "3306"
+  type        = "string"
+  default     = "3306"
+  description = "The port on which to accept connections"
 }
 
 variable "apply_immediately" {
-  default = "false"
+  type        = "string"
+  default     = "false"
+  description = "Determines whether or not any DB modifications are applied immediately, or during the maintenance window"
 }
 
 variable "monitoring_interval" {
-  default = 0
+  type        = "string"
+  default     = 0
+  description = "The interval (seconds) between points when Enhanced Monitoring metrics are collected"
 }
 
 variable "auto_minor_version_upgrade" {
-  default = "false"
+  type        = "string"
+  default     = "false"
+  description = "Determines whether minor engine upgrades will be performed automatically in the maintenance window"
 }
 
 variable "db_parameter_group_name" {
-  default = "default.aurora5.6"
+  type        = "string"
+  default     = "default.aurora5.6"
+  description = "The name of a DB parameter group to use"
 }
 
 variable "db_cluster_parameter_group_name" {
-  default = "default.aurora5.6"
+  type        = "string"
+  default     = "default.aurora5.6"
+  description = "The name of a DB Cluster parameter group to use"
 }
 
 variable "snapshot_identifier" {
-  default = ""
+  type        = "string"
+  default     = ""
+  description = "DB snapshot to create this database from"
 }
 
 variable "storage_encrypted" {
-  default = "true"
+  type        = "string"
+  default     = "true"
+  description = "Specifies whether the underlying storage layer should be encrypted"
 }
 
 variable "cw_alarms" {
-  default = false
+  type        = "string"
+  default     = false
+  description = "Whether to enable CloudWatch alarms - requires `cw_sns_topic` is specified"
 }
 
 variable "cw_sns_topic" {
-  default = "false"
+  type        = "string"
+  default     = "false"
+  description = "An SNS topic to publish CloudWatch alarms to"
 }
 
 variable "cw_max_conns" {
-  default = "500"
+  type        = "string"
+  default     = "500"
+  description = "Connection count beyond which to trigger a CloudWatch alarm"
 }
 
 variable "cw_max_cpu" {
-  default = "85"
+  type        = "string"
+  default     = "85"
+  description = "CPU threshold above which to alarm"
 }
 
 variable "cw_max_replica_lag" {
-  default = "2000"
+  type        = "string"
+  default     = "2000"
+  description = "Maximum Aurora replica lag in milliseconds above which to alarm"
 }
