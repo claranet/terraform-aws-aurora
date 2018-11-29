f4c7aa934cb01045d1dc298068125c41ecd975a1
Alan Jenkins <alan.jenkins@claranet.uk>
2018-11-29 10:32:12 +0000

    Add IAM database authentication support
    Add IAM database authentication support as per:
    https://www.terraform.io/docs/providers/aws/r/rds_cluster.html#iam_database_authentication_enabled

--- tests/terraform/test-mysql-57-autoscaling.tf
+++ tests/terraform/test-mysql-57-autoscaling.tf
@@ -3,33 +3,34 @@ resource "aws_sns_topic" "db_alarms_57_autoscaling" {
 }
 
 module "aurora_db_57_autoscaling" {
-  source                          = "../.."
-  engine                          = "aurora-mysql"
-  engine-version                  = "5.7.12"
-  name                            = "test-aurora-db-57-autoscaling"
-  envname                         = "test-57-autoscaling"
-  envtype                         = "test"
-  subnets                         = ["${module.vpc.private_subnets}"]
-  azs                             = ["${module.vpc.availability_zones}"]
-  security_groups                 = ["${aws_security_group.allow_all.id}"]
-  instance_type                   = "db.t2.medium"
-  username                        = "root"
-  password                        = "changeme"
-  backup_retention_period         = "5"
-  final_snapshot_identifier       = "final-db-snapshot-prod"
-  storage_encrypted               = "true"
-  apply_immediately               = "true"
-  monitoring_interval             = "10"
-  cw_alarms                       = true
-  cw_sns_topic                    = "${aws_sns_topic.db_alarms_57_autoscaling.id}"
-  db_parameter_group_name         = "${aws_db_parameter_group.aurora_db_57_autoscaling_parameter_group.id}"
-  db_cluster_parameter_group_name = "${aws_rds_cluster_parameter_group.aurora_57_autoscaling_cluster_parameter_group.id}"
-  replica_scale_enabled           = true
-  replica_scale_min               = "1"
-  replica_scale_max               = "1"
-  replica_scale_cpu               = "70"
-  replica_scale_in_cooldown       = "300"
-  replica_scale_out_cooldown      = "300"
+  source                              = "../.."
+  engine                              = "aurora-mysql"
+  engine-version                      = "5.7.12"
+  name                                = "aurora-my57-asg"
+  envname                             = "test-57-asg"
+  envtype                             = "test"
+  subnets                             = ["${module.vpc.private_subnets}"]
+  azs                                 = ["${module.vpc.availability_zones}"]
+  security_groups                     = ["${aws_security_group.allow_all.id}"]
+  instance_type                       = "db.t2.medium"
+  username                            = "root"
+  password                            = "changeme"
+  backup_retention_period             = "5"
+  final_snapshot_identifier           = "final-db-snapshot-prod"
+  storage_encrypted                   = "true"
+  apply_immediately                   = "true"
+  monitoring_interval                 = "10"
+  cw_alarms                           = true
+  cw_sns_topic                        = "${aws_sns_topic.db_alarms_57_autoscaling.id}"
+  db_parameter_group_name             = "${aws_db_parameter_group.aurora_db_57_autoscaling_parameter_group.id}"
+  db_cluster_parameter_group_name     = "${aws_rds_cluster_parameter_group.aurora_57_autoscaling_cluster_parameter_group.id}"
+  replica_scale_enabled               = true
+  replica_scale_min                   = "1"
+  replica_scale_max                   = "1"
+  replica_scale_cpu                   = "70"
+  replica_scale_in_cooldown           = "300"
+  replica_scale_out_cooldown          = "300"
+  iam_database_authentication_enabled = "true"
 }
 
 resource "aws_db_parameter_group" "aurora_db_57_autoscaling_parameter_group" {
