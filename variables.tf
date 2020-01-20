c4a5c15ac4b3df68aa4e30ba2fc596d9a2ce31a1
Jan Ritter <jan.ritter@trustedshops.de>
2020-01-20 15:38:41 +0100

    refactor: removed unnecessary quotes from deletion protection default variable

--- variables.tf
+++ variables.tf
@@ -130,7 +130,7 @@ variable "db_cluster_parameter_group_name" {
 
 variable "deletion_protection" {
   type        = string
-  default     = "false"
+  default     = false
   description = "Determines if the DB instance should have deletion protection enabled. The database can't be deleted when this value is set to true. The default is false."
 }
 
