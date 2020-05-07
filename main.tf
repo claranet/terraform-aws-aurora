5d638f64baf329aa4206a2d85352281adb6e177d
Michal Bolek <michal@blockv.io>
2020-05-07 12:29:21 +0200

    Add tags to aws_rds_cluster

--- main.tf
+++ main.tf
@@ -89,6 +89,11 @@ resource "aws_rds_cluster" "default" {
   db_cluster_parameter_group_name     = var.db_cluster_parameter_group_name
   iam_database_authentication_enabled = var.iam_database_authentication_enabled
   enabled_cloudwatch_logs_exports     = var.enabled_cloudwatch_logs_exports
+
+  tags = merge({
+    envname = var.envname
+    envtype = var.envtype
+  }, var.extra_tags)
 }
 
 // Geneate an ID when an environment is initialised
